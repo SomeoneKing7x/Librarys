84e09c584cc354f356efb32e39aba021ab8a44a9
Someone <lucasviniciussoaresdasilva14@gmail.com>
2025-03-21 19:47:57 -0300

    Update RedzLibExemple.lua

--- RedzLibExemple.lua
+++ RedzLibExemple.lua
@@ -1,1 +1,12 @@
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/SomeoneKing7x/Librarys/refs/heads/main/RedzLib.lua"))()
+
+local Window = redzlib:MakeWindow({
+  Title = "Name Hub : GameTest",
+  SubTitle = "by Name Hub",
+  SaveFolder = "NameHub.lua"
+})
+
+local Tab1 = Window:MakeTab({"Um", "cherry"})
+local Tab2 = Window:MakeTab({"Dois", "swords"})
+local Tab3 = Window:MakeTab({"TrÃªs", "user"})
 
